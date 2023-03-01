51427f05e63bd6ad7bc11702343dd91da9805a34
makiesse <ddociel@hotmail.com>
2023-03-01 00:20:55 -0300

    installing development tools

---
+++ src/main/java/br/com/springboot/makiesse_java_engineer/Application.java
@@ -0,0 +1,15 @@
+package br.com.springboot.makiesse_java_engineer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args); /*It is the main thread that runs the Java Spring Boot project*/
+    }
+}
