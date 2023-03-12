efc158e417299c29779e2617e9c0dcb22deaa3d1
makiesse <ddociel@hotmail.com>
2023-03-12 02:53:19 -0300

    Create Model and table in the database

--- src/main/java/br/com/springboot/makiesse_java_engineer/Application.java
+++ src/main/java/br/com/springboot/makiesse_java_engineer/Application.java
@@ -2,14 +2,16 @@ package br.com.springboot.makiesse_java_engineer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 /**
  *
  * Spring Boot application starter class
  */
+@EntityScan(basePackages = "br.springboot.makiesse_java_engineer.model")
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args); /*It is the main thread that runs the Java Spring Boot project*/
+        SpringApplication.run(Application.class, args); /*É a linha principal que roda o projeto Java Spring Boot*/
     }
 }
--- src/main/java/br/com/springboot/makiesse_java_engineer/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/makiesse_java_engineer/controllers/GreetingsController.java
@@ -18,9 +18,16 @@ public class GreetingsController {
      * @param name the name to greet
      * @return greeting text
      */
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
+    @RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET) //Mapeamento de requisição
     @ResponseStatus(HttpStatus.OK)
     public String greetingText(@PathVariable String name) {
         return " Welcome to the first CTD " + name + "!";
+    }
+    
+    @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String retornaOlaMundo(@PathVariable String nome) {
+    	
+    	return "Olá mundo " + nome;
     }
 }
