588204a6ad1ed86e226883240aa21db9915fff86
makiesse <ddociel@hotmail.com>
2023-03-14 20:01:07 -0300

    Create the END-POINT to save in the database

--- src/main/java/br/com/springboot/makiesse_java_engineer/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/makiesse_java_engineer/controllers/GreetingsController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -58,4 +60,21 @@ public class GreetingsController {
     	
     }
     
+    @PostMapping(value = "salvar")/*Mapeia a url*/
+    @ResponseBody /*Descrição da resposta*/
+    public ResponseEntity<Usuario> salvar (@RequestBody Usuario usuario){ /*Receber os dados para salvar*/
+    	
+    	Usuario user = usuarioRepository.save(usuario);
+		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+    }
+    
 }
+
+
+
+
+
+
+
+
+
