9ad156fbe59fc6add2bc00b1fb8d7ba7b8013848
makiesse <ddociel@hotmail.com>
2023-03-14 20:34:40 -0300

    Create END-POINT for bank DELETE

--- src/main/java/br/com/springboot/makiesse_java_engineer/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/makiesse_java_engineer/controllers/GreetingsController.java
@@ -3,12 +3,14 @@ package br.com.springboot.makiesse_java_engineer.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -68,11 +70,20 @@ public class GreetingsController {
 		return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
     }
     
+    
+    
+    @DeleteMapping(value = "delete")/*Mapeia a url*/
+    @ResponseBody /*Descrição da resposta*/
+    public ResponseEntity<String> delete (@RequestParam Long iduser){ /*Receber os dados para salvar*/
+    	
+    	 usuarioRepository.deleteById(iduser);
+    	 
+		return new ResponseEntity<String>("User deletado com sucesso",  HttpStatus.OK);
+    
 }
 
 
-
-
+}
 
 
 
